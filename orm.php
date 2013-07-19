e5d0ad1ae8be254d9b567f29b8f87a206fb29844
Richard Andrew Cattermole <pbdadmin@gmail.com>
2013-07-19 18:39:24 +1200

    Initial commit from dev

---
+++ orm.php
@@ -0,0 +1,262 @@
+<?php
+namespace ORM;
+/*
+* Copyright (c) 2013 Richard Andrew Cattermole
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
+*/
+
+if (!defined('GlobalDBName'))
+    define('GlobalDBName', 'pdo');
+
+class Model {
+    private $cameFromDb = false;
+    private $updateStatement;
+    private $insertStatement;
+    private $db;
+    
+    protected static $ids = array();
+    
+    public function __construct($db = null) {
+        if ($db == null && isset($GLOBALS[GlobalDBName])) {
+            $db = $GLOBALS[GlobalDBName];
+        }
+        if ($db != null) {
+            $this->db = $db;
+            $names = '';
+            $names2 = '';
+            $updateSets = '';
+            $where = '';
+            
+            foreach(class_property_names($this) as $prop) {
+                if (!in_array($prop, static::$ids)) {
+                    $names .= $prop . ', ';
+                    $names2 .= ':' . $prop . ', ';
+                    $updateSets .= $prop . '=:' .$prop . ',';
+                }
+            }
+            if (count_chars($names) > 0) {
+                $names = substr($names, 0, -2);
+                $names2 = substr($names2, 0, -2);
+                $updateSets = substr($updateSets, 0, -1);
+            }
+            
+            foreach(static::$ids as $id) {
+                $where .= $id . '=:' . $id . ' AND ';
+            }
+            if (count_chars($where) > 0) {
+                $where = substr($where, 0, -5);
+            }
+            
+            $className = explode('\\', get_class($this));
+            $className = array_pop($className);
+            
+            $this->updateStatement = $db->prepare('UPDATE ' . $className . ' SET ' . $updateSets . ' WHERE ' . $where);
+            $this->insertStatement = $db->prepare('INSERT INTO ' . $className . '(' . $names . ') VALUES(' . $names2 . ')');
+        } else {
+            throw new \Exception('Tried getting a database connection and failed - none existed');
+        }
+    }
+    
+    public function save() {
+        $values = class_values($this);
+        // save the data to the given database.
+        $this->db->beginTransaction();
+        try {
+            if ($this->cameFromDb) {
+                foreach($values as $key => $value) {
+                    $this->updateStatement->bindValue(':' . $key, $value);
+                }
+                $this->updateStatement->execute();
+                if ($this->updateStatement->errorCode() !== '00000')
+                    throw new \Exception('Could not insert into database' . serialize($this->updateStatement->errorInfo()));
+            } else {
+                foreach($values as $key => $value) {
+                    if (!in_array($key, static::$ids)) {
+                        $this->insertStatement->bindValue(':' . $key, $value);
+                    }
+                }
+                $this->insertStatement->execute();
+                if ($this->insertStatement->errorCode() !== '00000')
+                    throw new \Exception('Could not insert into database' . serialize($this->insertStatement->errorInfo()));
+                
+                if($this->insertStatement->rowCount() == 1) {
+                    foreach($values as $key => $value) {
+                        if (in_array($key, static::$ids)) {
+                            $value = $this->db->lastInsertId($key);
+                            $this->$key = $value;
+                        }
+                    }
+                }
+                $this->hasComeFromDB();
+            }
+             $this->db->commit();
+        } catch (Exception $e) {
+            $this->db->rollback();
+            throw $e;
+        }
+    }
+    
+    public function delete() {
+        $className = explode('\\', get_class($this));
+        $className = array_pop($className);
+                
+        $where = '';
+        foreach(static::$ids as $id) {
+            $where .= $id . '=:' . $id . ' AND ';
+        }
+        if (count_chars($where) > 0) {
+            $where = substr($where, 0, -5);
+        }
+                
+        // array of id values
+        $stmt = $this->db->prepare('DELETE from ' . $className . ' where ' . $where);
+        foreach(class_values($this) as $key => $value) {
+            if (in_array($key, static::$ids)) {
+                $stmt->bindValue(':' . $key, $value);
+            }
+        }
+        $stmt->execute();
+        if ($stmt->errorCode() !== '00000')
+            throw new \Exception('Could not delete from database' . serialize($stmt->errorInfo()));
+    }
+    
+    public function hasComeFromDB() {
+        $this->cameFromDb = true;
+    }
+    
+    public static function create($values) {
+        return class_new(get_called_class(), $values, false);
+    }
+    
+    public static function get($sqlOrIds, $sqlParams=array(), $db=null) {
+        $ret = null;
+        
+        if ($db == null && isset($GLOBALS[GlobalDBName])) {
+            $db = $GLOBALS[GlobalDBName];
+        }
+        if ($db != null) {
+            if (gettype($sqlOrIds) == 'array') {
+                $className = explode('\\', get_called_class());
+                $className = array_pop($className);
+                
+                $where = '';
+                foreach(static::$ids as $id) {
+                    $where .= $id . '=:' . $id . ' AND ';
+                }
+                if (count_chars($where) > 0) {
+                    $where = substr($where, 0, -5);
+                }
+                
+                // array of id values
+                $stmt = $db->prepare('SELECT * from ' . $className . ' where ' . $where);
+                foreach ($sqlOrIds as $key => $value) {
+                    if (gettype($key) == 'string') {
+                        $stmt->bindValue(':' . $key, $value);
+                    } else {
+                        $stmt->bindValues($key, $value);
+                    }
+                }
+                $stmt->execute();
+                if ($stmt->errorCode() !== '00000')
+                    throw new \Exception('Could not insert into database' . serialize($stmt->errorInfo()));
+                
+                $ret = $stmt->fetchObject(get_called_class());
+            } else if (gettype($sqlOrIds) == 'string') {
+                // string aka sql
+                $stmt = $db->prepare($sqlOrIds);
+                foreach ($sqlParams as $key => $value) {
+                    $stmt->bindValue($key, $value);
+                }
+                $stmt->execute();
+                if ($stmt->errorCode() !== '00000')
+                    throw new \Exception('Could not insert into database' . serialize($stmt->errorInfo()));
+                
+                $ret = $stmt->fetchObject(get_called_class());
+            }
+        } else {
+            throw new \Exception('Tried getting a database connection and failed - none existed');
+        }
+        
+        if ($ret != null) {
+            $ret->hasComeFromDB();
+        }
+        
+        return $ret;
+    }
+}
+
+$registered_classes = array();
+
+function class_add($name) {
+    global $registered_classes;
+    $registered_classes[$name] = new \ReflectionClass($name);
+}
+
+function class_new($name, $values=null, $fromDb = false) {
+    global $registered_classes;
+    $reflect = $registered_classes[$name];
+    $instance = $reflect->newInstance();
+    
+    if (gettype($values) == 'array') {
+        foreach($values as $key => $value) {
+            $reflect->getProperty($key)->setValue($instance, $value);
+        }
+    }
+    
+    if ($fromDb) {
+        $instance->hasComeFromDB();
+    }
+    
+    return $instance;
+}
+
+function class_values($obj) {
+    global $registered_classes;
+    $name = get_class($obj);
+    $reflect = $registered_classes[$name];
+    
+    $ret = array();
+    
+    foreach($reflect->getProperties() as $prop) {
+        if ($prop->isPublic() && !$prop->isStatic()) {
+            $propName = $prop->getName();
+            $ret[$propName] = $prop->getValue($obj);
+        }
+    }
+    
+    return $ret;
+}
+
+function class_property_names($obj) {
+    global $registered_classes;
+    $name = get_class($obj);
+    $reflect = $registered_classes[$name];
+    
+    $ret = array();
+    
+    foreach($reflect->getProperties() as $prop) {
+        if ($prop->isPublic() && !$prop->isStatic()) {
+            $ret[] = $prop->getName();
+        }
+    }
+    
+    return $ret;
+}
+?>
